0f97cd1cdedeec727af713e7d33c351427bbbfe5
it21998 <79991769+it21998@users.noreply.github.com>
2022-06-04 13:54:32 +0300

    Update settings.py

--- student_management/student_management/settings.py
+++ student_management/student_management/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/student_management/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 AUTH_USER_MODEL="student_management_app.CustomUser"
 AUTHENTICATION_BACKENDS=['student_management_app.EmailBackEnd.EmailBackEnd']
 
