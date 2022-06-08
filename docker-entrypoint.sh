54399782ab8615eb8d4fdc294235313d0ac40d46
it21998 <79991769+it21998@users.noreply.github.com>
2022-06-08 15:16:04 +0300

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Collect static files
-echo "Collect static files"python manage.py collectstatic --noinput
-# Apply database migrations
-echo "Apply database migrations"python manage.py migrate
